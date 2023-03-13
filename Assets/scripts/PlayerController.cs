76e7791820f82e0fb0d6001f52825c02362d63a1
Dylan-Walters-Student <112421363+Dylan-Walters-Student@users.noreply.github.com>
2023-03-13 12:24:41 -0400

    Ended on collision exit 2d

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +6,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float TorqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2d;
-    // Start is called before the first frame update
+    SurfaceEffector2D surfaceEffector2D;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(TorqueAmount);
         }
-
-        if (Input.GetKey(KeyCode.RightArrow)){
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
             rb2d.AddTorque(-TorqueAmount);
         }
     }
